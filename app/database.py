5a2ea7fa6a895b440690a93dd3ff1e2e044f7aae
WangLJ-SEU <827006477@qq.com>
2024-12-20 21:56:22 +0800

    safe database

--- app/database.py
+++ app/database.py
@@ -3,8 +3,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:braintell%40seu@localhost/human_neuron'
-# SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:hneuronbyseu123@localhost/human_neuron"
+SQLALCHEMY_DATABASE_URL = ''
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
